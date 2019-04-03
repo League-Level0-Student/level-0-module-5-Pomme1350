e227fa564ab07890e89aa6318dcc0be97bca0dd9
league <league@wts9.attlocal.net>
2019-04-03 16:56:23 -0700

    almost don.............

--- src/nested_loops/CircleRing.java
+++ src/nested_loops/CircleRing.java
@@ -7,29 +7,41 @@ package nested_loops;
 import org.jointheleague.graphical.robot.Robot;
 
 public class CircleRing {
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 
-        // 1. Make a new Robot
+		// 1. Make a new Robot
+		Robot r2d2 = new Robot();
 
+		// 2. Set your robot’s position to x=150 and y=200
+		r2d2.setX(150);
+		r2d2.setY(200);
+		// 3. Put the robot's pen down
+		r2d2.penDown();
+		// 4. Set the robot’s speed to 10
+		r2d2.setSpeed(200);
+		// 5. Do everything below here 360 times (use i as the counter)
+		for (int i = 0; i < 360; i++) {
+			if (i % 20 ==0 ) {
+				for(int j=0; j<360;i++) {
+					r2d2.move(1);
+					r2d2.turn(1);
+				}
+				}
+			r2d2.move(3);
+			r2d2.turn(1);
+			 
+				
+		}
+		// 6. Move the robot 3 pixels
 
-       //  2. Set your robot’s position to x=150 and y=200
+		// 7. Turn the robot 1 degree
 
-        // 3. Put the robot's pen down
-
-        // 4. Set the robot’s speed to 10
-
-        // 5. Do everything below here 360 times (use i as the counter)
-
-	        // 6. Move the robot 3 pixels
-
-       //  7. Turn the robot 1 degree
-
-        // 8. If the counter i is divisible by 20 (hint: use mod operator  %)..
-
+		// 8. If the counter i is divisible by 20 (hint: use mod operator %)..
+		
 		// 9. do steps 10 and 11 exactly 360 times (use j as the counter)
 
-				// 10. Move the robot 1 pixel
+		// 10. Move the robot 1 pixel
 
-				// 11. Turn the robot 1 degree
-}
+		// 11. Turn the robot 1 degree
+	}
 }
---
+++ src/randomrobot.java
@@ -0,0 +1,10 @@
+import org.jointheleague.graphical.robot.Robot;
+
+public class randomrobot {
+public static void main(String[] args) {
+	Robot r2d2 = new Robot();
+	Robot r2d22 = new Robot();
+	r2d22.setX(100);
+	r2d22.setY(100);
+}
+}
